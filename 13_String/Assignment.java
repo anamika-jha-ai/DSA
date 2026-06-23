9c753eb443ceb3d14d4f95c0a600a69f218d479f
Anamika Jha <anamika.jha.cse@gmail.com>
2026-06-23 18:32:29 +0530

    adding

--- 13_String/Assignment.java
+++ 13_String/Assignment.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 public class Assignment{
 
     public static void main(String args[]){
@@ -13,6 +13,34 @@ public class Assignment{
             }                
 
         }
-        System.out.print("count of vowels is :" + count);
+        System.out.println("count of vowels is :" + count);
+
+        //checkinf if the the strings are anagram
+       String str1 = "earth";
+       String  str2 = "heart";
+        //coverting to lowercase to comapre
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        //first check if length are same 
+        if(str1.length() == str2.length()){
+
+            //COVERTING THE STRING TO CHAR ARRAY
+            char[] str1charArray = str1.toCharArray();
+             char[] str2charArray =str2.toCharArray();
+
+            //SORTING THE CHAR ARRAY        
+             Arrays.sort(str1charArray);
+             Arrays.sort(str2charArray);
+
+            boolean result = Arrays.equals(str1charArray , str2charArray);
+            if(result){
+                System.out.println("BOTH STRINGS ARE ANAGRAM");
+            }else{
+                System.out.println("ARENOT ANAGRAM");
+            }
+
+        }else{
+            System.out.println(str1 +" "+ "and" +" "+ str2 +" "+ "are not anagrams");
+        }
     }
 }
