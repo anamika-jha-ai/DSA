083dd7256f500caf6dbc29fd5b3907f23db95d15
Anamika Jha <anamika.jha.cse@gmail.com>
2026-06-28 16:29:37 +0530

    adding dsa

---
+++ 13_String/stringCompression.java
@@ -0,0 +1,20 @@
+
+public class stringCompression{
+
+    public static void main(String args[]){
+        String str = "aaabcc";
+        String newstr="";
+        for(int i =0 ; i<str.length(); i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i)== str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newstr += str.charAt(i);
+            if(count >1){
+                newstr += count.toString();
+            }
+        }
+       System.out.println(newstr);
+    }
+}
