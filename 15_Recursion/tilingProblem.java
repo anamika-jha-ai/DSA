00296de4fd0f9ed342d8544d05043484eed0c073
Anamika Jha <anamika.jha.cse@gmail.com>
2026-07-10 14:27:07 +0530

    add

---
+++ 15_Recursion/tilingProblem.java
@@ -0,0 +1,19 @@
+public class tilingProblem{
+
+    public static int tilingProblem(int n){
+
+        //base case
+        if(n==0|| n==1){
+            return 1;
+        }
+        //vertical choice
+        int fnm1 = tilingProblem(n-1);
+        //horizontal choice
+        int fnm2 = tilingProblem(n-2);
+        return fnm1 + fnm2;//total ways to tile the floor
+    }
+
+    public static void main(String args[]){
+        System.out.println(tilingProblem(2));
+    }
+}
