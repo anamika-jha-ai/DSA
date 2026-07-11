16cd0f591f3461f0f260439849ce0ffe3a037bdf
Anamika Jha <anamika.jha.cse@gmail.com>
2026-07-11 17:06:47 +0530

    add

---
+++ 15_Recursion/friendPairing.java
@@ -0,0 +1,21 @@
+public class friendPairing{
+
+    public static int friendPairing(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+
+        //single choice
+          //     int fnm1 = friendPairing(n-1);
+          //     //pair choice
+          //     int fnm2 = friendPairing(n-2);
+          //    int pairWays = (n-1) * fnm2;
+          //     return fnm1 + pairWays;
+
+        return friendPairing(n-1) + (n-1) * friendPairing(n-2);
+    }
+
+    public static void main(String args[]){
+        System.out.println(friendPairing(2));
+    }
+}
